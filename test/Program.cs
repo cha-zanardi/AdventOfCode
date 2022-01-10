e09d9218091c21030d7f240c94e3fa28747c5eb4
Chacha <cha.zanardi@gmail.com>
2022-01-10 14:55:00 +0100

    finished day3_part2 added test

---
+++ test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+Console.WriteLine(10 / 2);
+Console.WriteLine(10 / 5);
+Console.WriteLine(10 / 10);
+
